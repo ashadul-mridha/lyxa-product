94f34ebee02451757be34e992e92e9666cdd8c1a
Ashadul Islam <ashadulmridhaprog@gmail.com>
2025-05-29 21:25:41 +0600

    feat(package): install required package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // swagger implement
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Lyxa Product API')
+    .setDescription('Lyxa Product API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        bearerFormat: 'JWT',
+      },
+      'JWT',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
