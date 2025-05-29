8d4697e309061e2aff607686efdd2cd2ac68745d
Ashadul Islam <ashadulmridhaprog@gmail.com>
2025-05-29 22:13:17 +0600

    feat(rabbitmq): rabbitmq setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
+import { RabbitmqModule } from './config/rabbitmq/rabbitmq.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AppConfigModule } from './config/app/config.module';
       envFilePath: '.env',
     }),
     MongooseModule.forRoot(process.env.DB_URL),
+    RabbitmqModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/config/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,31 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { Module } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { RabbitmqService } from './rabbitmq.service';
+dotenv.config();
+
+@Module({
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'booking_api',
+          type: 'topic',
+          // type: 'fanout',
+        },
+        {
+          name: 'booking_rpc',
+          type: 'direct',
+        },
+      ],
+      uri: process.env.RMQ_URL,
+      connectionInitOptions: {
+        wait: false,
+      },
+      enableControllerDiscovery: true,
+    }),
+  ],
+  exports: [RabbitMQModule, RabbitmqService],
+  providers: [RabbitmqService],
+})
+export class RabbitmqModule {}
